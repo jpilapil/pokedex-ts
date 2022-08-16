c5e4ae99a375c1acaeac283e529881b36ef1d942
Justin Pilapil <justin.pilapil@yahoo.com>
2022-08-15 18:12:46 -0700

    add pokemon image and fix interface/types

--- components/Home/index.tsx
+++ components/Home/index.tsx
@@ -4,31 +4,48 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from './Home.module.css'
 
+/**
+ * TODO:
+ * [ ] Add dynamic page to view more about pokemon
+ */
 
 interface IPokemon {
    name: string;
    url?: string;
-   abilities: string;
+   image?: string;
+   abilities: {
+      ability: string;
+   }
+}
+
+type TAbility = {
+   ability: {
+      name: string;
+   };
 }
 
 const Home: NextPage = () => {
    const [pokemonName, setPokemonName] = useState('')
    const [pokemonResponse, setPokemonResponse] = useState({
       name: '',
-      abilities: []
+      abilities: [],
+      image: ''
    })
 
 
-   const fetchPokemon = async (name: string) => {
+   const fetchPokemon = async (name: string): Promise<IPokemon> => {
       const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
       const result = await response.json()
-      const pokemonAbilities = result.abilities.map((item: any) => {
+      console.log(result)
+      const pokemonAbilities = result.abilities.map((item: TAbility) => {
          return item.ability.name
       })
-      setPokemonResponse({name: result.name, abilities: pokemonAbilities})
-      return pokemonResponse;
+      const pokemonImage = result.sprites.front_default;
+      // set desired results to state to access on the webpage
+      setPokemonResponse({name: result.name, abilities: pokemonAbilities, image: pokemonImage})
+      return result;
    }
-   
+   ``
   return (
       <div className={styles.container}>
          <h1>Search a Pokemon</h1>
@@ -39,10 +56,16 @@ const Home: NextPage = () => {
             fetchPokemon(pokemonName)
          }}>Search Pokemon</button>
          <h1>Name: {pokemonResponse.name}</h1>
+         <Image
+            src={pokemonResponse.image}
+            alt={pokemonResponse.name}
+            width={200}
+            height={200}
+         />
          <p>Abilities:</p>
          <ul>
-         {pokemonResponse.abilities.map((ability) => (
-            <li>{ability}</li>
+         {pokemonResponse.abilities.map((ability, i) => (
+            <li key={i}>{ability}</li>
          ))}
          </ul>
       </div>
